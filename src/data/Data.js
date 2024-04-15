f1c04027bcc6052340a07a63648456158692fd5c
bmancilla23 <bmancilla119@gmail.com>
2024-04-15 04:18:33 -0500

    Creación de data

---
+++ src/data/Data.js
@@ -0,0 +1,84 @@
+import defaultImage from '@/assets/default_image.jpg'
+import Work1 from '@/assets/work1.jpg'
+import Work2 from '@/assets/work2.jpg'
+import Work3 from '@/assets/work3.jpg'
+import Work4 from '@/assets/work4.jpg'
+import Work5 from '@/assets/work5.jpg'
+
+export const projectsData = [
+    {
+      id: 1,
+      title: "Project 1",
+      image: Work1,
+      category: "react"
+    },
+    {
+      id: 2,
+      title: "Project 2",
+      image: Work2,
+      category: "angular"
+    },
+    {
+      id: 3,
+      title: "Project 3",
+      image: Work3,
+      category: "full stack"
+    },
+    {
+      id: 4,
+      title: "Project 4",
+      image: Work4,
+      category: "react"
+    },
+    {
+      id: 5,
+      title: "Project 5",
+      image: Work5,
+      category: "angular"
+    },
+    {
+      id: 6,
+      title: "Project 6",
+      image: defaultImage,
+      category: "full stack"
+    },
+    {
+      id: 7,
+      title: "Project 7",
+      image: defaultImage , 
+      category: "react"
+    },
+    {
+      id: 8,
+      title: "Project 8",
+      image:  defaultImage,
+      category: "angular"
+    },
+    {
+      id: 9,
+      title: "Project 9",
+      image: defaultImage,
+      category: "full stack"
+    },
+    {
+      id: 10,
+      title: "Project 10",
+      image: defaultImage,
+      category: "react"
+    }
+  ]
+
+  export const projectsNav = [
+    {
+        name: 'all',
+    },
+    {
+        name: 'react'
+    },
+    {
+        name: 'angular'
+    },
+    {
+        name: 'full-stack'
+    }
+  ]
